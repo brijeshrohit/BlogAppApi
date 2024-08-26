72254625528985a34b2c888a11e1a3fe12e68a24
Brijesh Rohit <brijeshrohit1.7@gmail.com>
2024-08-27 00:13:57 +0530

    User added

---
+++ src/main/java/com/brijesh/blog/entities/User.java
@@ -0,0 +1,30 @@
+package com.brijesh.blog.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "user_name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "about", length = 300)
+    private String about;
+}
